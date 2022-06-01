4070673204026d593ab239ae42aa7387eb1375f7
arteemm <arteemmarteemm@gmail.com>
2022-06-01 18:01:05 +0300

    init: momentum

--- src/components/index.js
+++
@@ -1,7 +0,0 @@
-import Clock from './Clock';
-import Weather from './Weather';
-import Background from './Background';
-import Quotes from './Quotes';
-import AudioPlayer from './AudioPlayer';
-
-export { Clock, Weather, Background, Quotes, AudioPlayer };
--- src/index.js
+++
@@ -1,5 +0,0 @@
-import './styles/style.scss';
-import App from './App';
-
-const app = new App();
-app.render();
