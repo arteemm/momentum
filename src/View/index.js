36ab910cb8f7a752c5bf030a9824198ce1354434
arteemm <arteemmarteemm@gmail.com>
2023-03-18 15:52:55 +0300

    fix: propblem export

--- src/View/index.js
+++
@@ -1,7 +0,0 @@
-import ViewWeather from './ViewWeather';
-import ViewAudioPlayer from './ViewAudio';
-import ViewSettings from './ViewSettings';
-import ViewTodoItem from './ViewTodoItem';
-import ViewClock from './ViewClock';
-
-export { ViewWeather, ViewAudioPlayer, ViewSettings, ViewTodoItem, ViewClock };
